606b8d72fbcd1b8f4660d31b0f530e05a48cd58f
emperorrp <rahulpujari2919@gmail.com>
2023-05-20 13:53:33 +0530

    uploading backend

---
+++ backend/hardhat-starter-kit/hardhat.config.ts
@@ -0,0 +1,121 @@
+import * as dotenv from "dotenv"
+
+import type { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-chai-matchers"
+import "hardhat-deploy"
+import "hardhat-contract-sizer"
+import "@appliedblockchain/chainlink-plugins-fund-link"
+import "./tasks"
+
+dotenv.config()
+
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// optional
+const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
+const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
+
+// Your API key for Etherscan, obtain one at https://etherscan.io/
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            hardfork: "merge",
+            // If you want to do some forking set `enabled` to true
+            forking: {
+                url: MAINNET_RPC_URL,
+                blockNumber: Number(FORKING_BLOCK_NUMBER),
+                enabled: false,
+            },
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL !== undefined ? SEPOLIA_RPC_URL : "",
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
+        polygon: {
+            url: POLYGON_MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 137,
+        },
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            // npx hardhat verify --list-networks
+            sepolia: ETHERSCAN_API_KEY,
+            mainnet: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["APIConsumer", "KeepersCounter", "PriceConsumerV3", "RandomNumberConsumer"],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        feeCollector: {
+            default: 1,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.6.6",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
+    },
+    typechain: {
+        outDir: "typechain",
+        target: "ethers-v5",
+    },
+}
+
+export default config
